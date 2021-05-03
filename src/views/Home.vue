761dd5da4922ebe9b0d70b56eb7feddcc0acf419
William Olewnicki <wolewnicki@yahoo.com>
2021-05-03 18:13:15 -0400

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld msg="replace opening" />
   </div>
 </template>
 
