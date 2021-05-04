e050b5fc0870e0468819fd41d1ada77ce5fc0fcb
William Olewnicki <wolewnicki@yahoo.com>
2021-05-03 21:33:36 -0400

    started playing with vue and getting familiar with components

---
+++ src/components/testComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div>
+      <ul>
+        <li>
+          <div class="square pfp box">
+            <img src="../assets/logo.png" />
+          </div>
+          <div class="box test">
+            <span class="above">Nico: </span>
+            <span class="below">{{ msg }}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <input placeholder="message Nico"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Test",
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped>
+
+img {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.square {
+  height: 75px;
+  width: 75px;
+}
+
+.box {
+  display: flex;
+}
+.below {
+  display: grid;
+  position: relative;
+  top: 2rem;
+  right: 2rem;
+}
+input {
+  width: 1000px;
+}
+</style>
---
+++ src/views/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Test msg="i am msg" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Test from "../components/testComponent.vue";
+
+export default defineComponent({
+  name: "Testview",
+  components: {
+    Test,
+  },
+});
+</script>
